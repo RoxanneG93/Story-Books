30c621cfb8800a907bd4793fd0b7bc20e972ca27
RoxanneG93 <Roxanne.Garcia4@my.scitexas.edu>
2018-06-19 16:01:35 -0500

    handled auth

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+
+// Load Routes
+const auth = require('./routes/auth');
+
+
+// USE Routes
+app.use('/auth', auth);
 
 // Routes
 app.get('/', (req, res) => {
 	res.send('It Works!');
 });
+
+
+// SERVER
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI: 'mongodb://RoxanneG93:gr8769@ds263500.mlab.com:63500/story_books',
+	googleClientID: '964800984642-bn71mdacsoq2igogb98hun9dsbgmu6pt.apps.googleusercontent.com',
+	googleClientSecret: 'oKxvy46Aei1xF6AbiZKjEPmE'
+}
